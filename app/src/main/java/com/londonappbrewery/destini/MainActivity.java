344747fc1a8582b0fb654e0b2d205974752662e6
hangphan91 <hang200280@yahoo.com>
2018-06-03 01:13:58 -0500

    completed!!

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,10 +2,18 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
+private TextView mTextStory;
+private Button mAnswer1;
+private Button mAnswer2;
+private int mStoryIndex;
 
 
     @Override
@@ -13,18 +21,90 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        if(savedInstanceState!=null){
+            mStoryIndex=savedInstanceState.getInt("IndexStory");
+        }else{
+           mStoryIndex=1;
+        }
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
-
+        mTextStory = (TextView) findViewById(R.id.storyTextView);
+        mAnswer1= (Button) findViewById(R.id.buttonTop);
+        mAnswer2= (Button) findViewById(R.id.buttonBottom);
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
-
+        mAnswer1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Destiny","Answer1 Clicked!!");
+                mTextStory.setText(R.string.T3_Story);
+                mAnswer1.setText(R.string.T3_Ans1);
+                mAnswer2.setText(R.string.T3_Ans2);
+                mStoryIndex=3;
+                update();
+            }
+        });
 
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+        mAnswer2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Destiny", "Answer 2 clicked!");
+                mTextStory.setText(R.string.T2_Story);
+                mAnswer1.setText(R.string.T2_Ans1);
+                mAnswer2.setText(R.string.T2_Ans2);
+                mStoryIndex=2;
+                update();
+            }
+        });
 
 
+    }
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("IndexStory", mStoryIndex);
+    }
+    public void update(){
+        if(mStoryIndex==2){
+            mAnswer1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mTextStory.setText(R.string.T3_Story);
+                    mAnswer1.setText(R.string.T3_Ans1);
+                    mAnswer2.setText(R.string.T3_Ans2);
+                    mStoryIndex=3;
+                    update();
+                }
+            });
+            mAnswer2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mTextStory.setText(R.string.T4_End);
+                    mAnswer1.setVisibility(View.GONE);
+                    mAnswer2.setVisibility(View.GONE);
+                }
+            });
+        }
+        if(mStoryIndex==3){
+            mAnswer1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mTextStory.setText(R.string.T6_End);
+                    mAnswer1.setVisibility(View.GONE);
+                    mAnswer2.setVisibility(View.GONE);
+                }
+            });
+            mAnswer2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mTextStory.setText(R.string.T5_End);
+                    mAnswer1.setVisibility(View.GONE);
+                    mAnswer2.setVisibility(View.GONE);
+                }
+            });
+        }
     }
 }
